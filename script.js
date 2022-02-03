5612d024c8695e8ff74c449c69bc4b6bc8498323
verenasworld <verena.felizidas@gmail.com>
2022-02-03 12:16:43 +0100

    add Calculation function html and style and first calc function

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-var counter = 0;
+//aufgabe 1
+let counter = 0;
 
 function incrementClick() {
     updateDisplay(++counter);
 }
 function updateDisplay(val) {
     document.getElementById("counter-label").innerHTML = val;
+}
+
+//Aufgabe 2
+
+let n1 = document.getElementById("number1");
+let n2 = document.getElementById("number2");
+let result = document.getElementById("result");
+
+function calculation(operator){
+  switch (operator){
+      case "+" :
+          return result = n1 + n2;
+          console.log(result)
+          break;
+      case "-" :
+          return result = n1 - n2;
+          break;
+      case "*" :
+          return result = n1 * n2;
+          break;
+      case "/" :
+          return result = n1 / n2;
+          break;
+  }
+
+
 }
