0de1fc74b90a60a2477e91ddb7e7b6193776a71f
verenasworld <verena.felizidas@gmail.com>
2022-02-03 18:44:29 +0100

    refactor html css and some js

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-
-
-
-//aufgabe 1
+//Task 1 onclick function
+//  click .firstbtn = h3 counter +1
 let counter = 0;
+
 function incrementClick() {
     updateDisplay(++counter);
 }
+
 function updateDisplay(val) {
-    document.getElementById("counter-label").innerHTML = val;
+    document.getElementById("counter").innerHTML = val;
 }
 
-//Aufgabe 2
-
-function calculation(operator){
+//Task 2 onclick Calc Function
+//
+function calculation(operator) {
     let n1 = parseInt(document.getElementById("number1").value);
     let n2 = parseInt(document.getElementById("number2").value);
     let result;
-    switch (operator){
+    switch (operator) {
         case "+" :
             result = n1 + n2;
             break;
         case "-" :
-             result = n1 - n2;
+            result = n1 - n2;
             break;
         case "*" :
-             result = n1 * n2;
+            result = n1 * n2;
             break;
         case "/" :
-             result = n1 / n2;
+            result = n1 / n2;
             break;
     }
-  document.getElementById('result').innerHTML = result;
+    document.getElementById('result').innerHTML = result;
 }
